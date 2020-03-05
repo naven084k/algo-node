423354a8e455a09128e19c5b2b6ff6e4477fbff5
naveenkumar.pokala <naveenkumar.pokala@servicenow.com>
2020-03-05 18:52:45 +0530

    test

--- linkedlist/FindNthNodeFromEnd.js
+++ linkedlist/FindNthNodeFromEnd.js
@@ -2,10 +2,10 @@ var x = require('./Node.js');
 
 let nthNode = process.argv[2];
 let head = x.prepareLinkedList();
-x.getSize(head);
+let size = x.getSize(head);
 
-if(nthNode > head)
-   throw console.error('not valid input to enter');
+if(nthNode > size)
+   throw 'not valid input to enter';
    
 x.printLinkedList(head);
 
