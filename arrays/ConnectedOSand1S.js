e7d58dd3db0a22af2ff020b94bffd7b16deef318
naveenkumar.pokala <naveenkumar.pokala@servicenow.com>
2020-03-16 19:55:18 +0530

    updated code

--- arrays/ConnectedOSand1S.js
+++ arrays/ConnectedOSand1S.js
@@ -1,9 +1,9 @@
 let arr = [
-  [1, 1, 0, 0, 0],
-  [0, 1, 1, 0, 0],
-  [0, 0, 1, 0, 1],
-  [1, 0, 1, 0, 1],
-  [0, 1, 1, 1, 1]
+  [1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1]
 ];
 
 let visited = [
@@ -29,8 +29,8 @@ for (let i = 0; i < arr.length; i++)
   for (let j = 0; j < arr[i].length; j++) {
     if (arr[i][j] == 1 && visited[i][j] == 0) {
       let t = findLengthOf1s(i, j);
-
-      if (max < t) max = t;
+      if (max < t)
+         max = t;
     }
   }
 
@@ -42,12 +42,14 @@ function findLengthOf1s(i, j) {
     let x = 0;
   
         for (let k = 0; k < traveller.length; k++) {
-            if (isInValidRange(i, j, traveller[k]) && visited[i + traveller[k][0]][j + traveller[k][1]] == 0) {
-            if (arr[i + traveller[k][0]][j + traveller[k][1]] == 1) {
-                let t = 1 + findLengthOf1s(i + traveller[k][0], j + traveller[k][1]);
 
-                if (x < t) x = t;
-            }
+            if (isInValidRange(i, j, traveller[k]) && visited[i + traveller[k][0]][j + traveller[k][1]] == 0) {
+
+                if (arr[i + traveller[k][0]][j + traveller[k][1]] == 1) {
+
+                    let t = 1 + findLengthOf1s(i + traveller[k][0], j + traveller[k][1]);
+                    if (x < t) x = t;
+                }
             }
         }
 
